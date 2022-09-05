a6473c43ad04f36f9a4c73dc021a7b866d13b67f
Aurélie Frémaux <aurelie.fremaux02@gmail.com>
2022-09-05 10:25:44 +0200

    mise en place du dossier

---
+++ config/index.php
@@ -0,0 +1,20 @@
+
+<!-- // create table matelas (
+//     id INT PRIMARY KEY AUTO_INCREMENT,
+//     marque varchar(50) not null,
+//         nom VARCHAR(50) NOT NULL,
+//         taille varchar(50) not null,
+//         prix DECIMAL(4,2)
+//     ) -->
+
+<!-- create table matelas (
+    id smallint primary key auto_increment,
+    image varchar(255),
+    marque varchar(50) not null,
+    name varchar(50) not null,
+//         taille varchar(50) not null,
+//         prix DECIMAL(8,4)
+
+) -->
+
+
